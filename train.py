2167ceff380335d525a8b1745354e58b802315fd
chocolate-i <402998484@qq.com>
2025-04-10 13:12:16 +0800

    ISIC2018成功运行版 -0.79

--- train.py
+++ train.py
@@ -53,10 +53,7 @@ def train_model(
         gradient_clipping: float = 1.0,
 ):
     # 1. Create dataset
-    try:
-        dataset = CarvanaDataset(dir_img, dir_mask, img_scale)
-    except (AssertionError, RuntimeError, IndexError):
-        dataset = BasicDataset(dir_img, dir_mask, img_scale)
+    dataset = CarvanaDataset(dir_img, dir_mask, img_scale)
 
     # 2. Split into train / validation partitions
     n_val = int(len(dataset) * val_percent)
--- utils/data_loading.py
+++ utils/data_loading.py
@@ -61,22 +61,15 @@ class BasicDataset(Dataset):
 
     @staticmethod
     def preprocess(mask_values, pil_img, scale, is_mask):
-        w, h = pil_img.size
-        newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
-        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+        fixed_size = (256, 256)  # 固定尺寸
+        pil_img = pil_img.resize(fixed_size, resample=Image.NEAREST if is_mask else Image.BICUBIC)
         img = np.asarray(pil_img)
 
         if is_mask:
-            mask = np.zeros((newH, newW), dtype=np.int64)
-            for i, v in enumerate(mask_values):
-                if img.ndim == 2:
-                    mask[img == v] = i
-                else:
-                    mask[(img == v).all(-1)] = i
-
-            return mask
-
+            # 将掩码值从 [0, 255] 映射到 [0, 1]
+            img = img / 255  # 归一化到 [0, 1]
+            img = img.astype(np.int64)  # 转换为整数类型
+            return img
         else:
             if img.ndim == 2:
                 img = img[np.newaxis, ...]
